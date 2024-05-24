f8cba4a59433888d5e8c98a7ffaf562504758637
Your Name <you@example.com>
2024-05-24 16:15:07 +0200

    Replace key placeholder in html

--- src/website/loadHtml.js
+++ src/website/loadHtml.js
@@ -1,6 +1,7 @@
-async function loadHtmlContent() {
+async function loadHtmlContent(keys) {
     /**
      * Lädt den HTML-Inhalt von einer URL
+     * @param {object} keys - Schlüssel-Wert-Paare (für die Saisons)
      * @returns {string} - HTML-Inhalt
      * @throws {Error} - Fehlermeldung, wenn der Netzwerk-Response nicht erfolgreich war
      * @throws {Error} - Fehlermeldung, wenn es ein Problem mit dem Fetch-Vorgang gab
@@ -11,7 +12,9 @@ async function loadHtmlContent() {
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
-        return await response.text();
+        let htmlContent = await response.text();
+        htmlContent = htmlContent.replace('<!-- KEYS_PLACEHOLDER -->', JSON.stringify(keys));
+        return htmlContent;
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
     }
