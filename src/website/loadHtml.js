26c2e531c1118d17b204388260ebf12dfae678db
Your Name <you@example.com>
2024-05-29 00:13:10 +0200

    Settings for colors

--- src/website/loadHtml.js
+++ src/website/loadHtml.js
@@ -1,4 +1,35 @@
-async function loadHtmlContent(keys, showStatistics) {
+const DEFAULT_SETTINGS = {
+    SHOW_STATISTICS: true,
+    PRIMARY_COLOR: '#31353E',
+    SECONDARY_COLOR: '#424959',
+    TERTIARY_COLOR: '#f2f4ef',
+    CONTRAST_COLOR: '#ffffff',
+    PRIMARY_BACKGROUND: '#f3f3f3',
+    SECONDARY_BACKGROUND: '#ffffff'
+}
+
+function replaceHtmlContent(htmlContent, keys, settings) {
+    /**
+     * Ersetzt die Placeholder im HTML-Code durch die eingestellten Werte
+     */
+
+    // Replace die Platzhalter für die Anzeige der Statistiken
+    htmlContent = htmlContent.replace('<!-- KEYS_PLACEHOLDER -->', JSON.stringify(keys));
+    // Replace die Platzhalter für die Anzeige der Statistiken
+    htmlContent = htmlContent.replace('<!-- DROPDOWN_LIST_TOP_PLACEHOLDER -->', settings.SHOW_STATISTICS ? '144px' : '76px');
+    htmlContent = htmlContent.replace('<!-- SELECTION_BUTTON_LIST_DISPLAY_PLACEHOLDER -->', settings.SHOW_STATISTICS ? 'flex' : 'none');
+    // Replace die Platzhalter für die Farben
+    htmlContent = htmlContent.replace('<!-- PRIMARY_COLOR_PLACEHOLDER -->', settings.PRIMARY_COLOR);
+    htmlContent = htmlContent.replace('<!-- SECONDARY_COLOR_PLACEHOLDER -->', settings.SECONDARY_COLOR);
+    htmlContent = htmlContent.replace('<!-- TERTIARY_COLOR_PLACEHOLDER -->', settings.TERTIARY_COLOR);
+    htmlContent = htmlContent.replace('<!-- CONTRAST_COLOR_PLACEHOLDER -->', settings.CONTRAST_COLOR);
+    htmlContent = htmlContent.replace('<!-- PRIMARY_BACKGROUND_PLACEHOLDER -->', settings.PRIMARY_BACKGROUND);
+    htmlContent = htmlContent.replace('<!-- SECONDARY_BACKGROUND_PLACEHOLDER -->', settings.SECONDARY_BACKGROUND);
+
+    return htmlContent; // Gib den HTML-Code zurück
+}
+
+async function loadHtmlContent(keys, settings) {
     /**
      * Lädt den HTML-Inhalt von einer URL
      * @param {object} keys - Schlüssel-Wert-Paare (für die Saisons)
@@ -7,20 +38,18 @@ async function loadHtmlContent(keys, showStatistics) {
      * @throws {Error} - Fehlermeldung, wenn es ein Problem mit dem Fetch-Vorgang gab
      */
 
-    if (showStatistics === undefined || showStatistics === null) showStatistics = true;
+    for (const key in DEFAULT_SETTINGS) {
+        if (!settings.hasOwnProperty(key)) {
+            settings[key] = DEFAULT_SETTINGS[key];
+        }
+    }
 
     try {
         let response = await fetch('https://raw.githubusercontent.com/nilskntl/dsv-club-leaderboards/master/src/website/index.html');
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
-        let htmlContent = await response.text();
-        // Replace die Keys im HTML-Inhalt
-        htmlContent = htmlContent.replace('<!-- KEYS_PLACEHOLDER -->', JSON.stringify(keys));
-        // Replace die Platzhalter für die Anzeige der Statistiken
-        htmlContent = htmlContent.replace('<!-- DROPDOWN_LIST_TOP_PLACEHOLDER -->', showStatistics ? '144px' : '76px');
-        htmlContent = htmlContent.replace('<!-- SELECTION_BUTTON_LIST_DISPLAY_PLACEHOLDER -->', showStatistics ? 'flex' : 'none');
-        return htmlContent;
+        return replaceHtmlContent(await response.text(), keys, settings);
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
     }
